e59d9584891b6f412d0bcd0f8a30c62c5ea2e067
BalazsBano <rhonadler@gmail.com>
2023-01-04 16:44:03 +0100

    create api with axios

---
+++ src/api/getPokemons/getPokemons.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const url: string = process.env.REACT_APP_API_URL || "";
+
+export async function getPokemons() {
+  try {
+    const data = await axios.get(url)
+    if (typeof data.data !== "object") {
+      return { data: "Intentional error", status: 500 };
+    } else {
+      return { data: data.data.results, status: data.status };
+    }
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return { data: error.message, status: 500 };
+    } else {
+      console.log("unexpected error: ", error);
+      return { data: "An unexpected error occured", status: 500 };
+    }
+  }
+}
