f23b04ebb27783de8ff22550c76d8e5efcd7c418
BalazsBano <rhonadler@gmail.com>
2023-01-05 17:07:35 +0100

    create modal for selected pokemon

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { Card, Dropdown, Modal } from "react-bootstrap";
-import { getPokemonTypes, getPokemonsByTypes } from "../../api";
+import { Button, Card, Dropdown, Modal } from "react-bootstrap";
+import { getPokemonTypes, getPokemonsByTypes, getSelectedPokemonsData } from "../../api";
+import { ISelectedPokemon } from "../../api/configuration";
 import { IPokemon } from "../../api/getPokemonTypes/types";
 import "./style.sass"
 
 export function HomePage(){
   const [pokemonTypes, setPokemonTypes] = useState([] as IPokemon[]);
   const [pokemonsByTypes, setPokemonsByTypes] = useState([] as IPokemon[]);
-  const [filteredPokemons, setFilteredPokemons] = useState([] as IPokemon[])
+  const [filteredPokemons, setFilteredPokemons] = useState([] as IPokemon[]);
+  const [selectedPokemon, setSelectedPokemon] = useState({} as ISelectedPokemon);
   const [errorMessageTypes, setErrorMessageTypes] = useState("");
   const [errorMessagePokemons, setErrorMessagePokemons] = useState("");
   const [searchInput, setSearchInput] = useState("");
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
+  const modalClose = () => setShow(false);
+	const modalShow = () => setShow(true);
 
   useEffect(() => {
     async function getData() {
@@ -34,7 +38,7 @@ export function HomePage(){
     } else {
       let pokemons = []
       for (let i = 0; i < pokemonsByTyp.data.length; i++) {
-        pokemons.push(pokemonsByTyp.data[i].pokemon)
+        pokemons.push({...pokemonsByTyp.data[i].pokemon, catch: false})
       }
       console.log(pokemons)
       setPokemonsByTypes(pokemons)
@@ -42,8 +46,36 @@ export function HomePage(){
     }
   }
 
-  async function viewPokemon(url: string){
-    console.log(url)
+  async function viewPokemon(url: string, name: string){
+    const catched = filteredPokemons.filter((pokemon) => {
+      return pokemon.name.match(name)
+    })[0].catch
+    
+    const pokemonData = await getSelectedPokemonsData(url)
+    if (pokemonData.status === 500) {
+      setErrorMessagePokemons(pokemonData.data);
+      console.log(pokemonData.status)
+    } else {
+      let abilities = [];
+      for (let i = 0; i < pokemonData.data.abilities.length; i++) {
+        if (pokemonData.data.abilities[i].is_hidden === false) {
+          abilities.push(pokemonData.data.abilities[i].ability.name)
+        }
+      }
+      
+      const selectedPokemonData = {
+        "name": name,
+        "image": pokemonData.data.sprites.other.home.front_default,
+        "weight": pokemonData.data.weight,
+        "height": pokemonData.data.height,
+        "abilities": abilities.join(", ")
+      }
+      setSelectedPokemon(selectedPokemonData)
+      console.log(selectedPokemonData)
+      setShow(true)
+    }
+
+    
   }
 
   function filteringPokemons(inputString: string){
@@ -56,6 +88,9 @@ export function HomePage(){
     }
   }
 
+  function catchingPokemon(name: string){
+    console.log(name)
+  }
 
   return (
     <div className="h-100 mt-3 d-flex flex-column justify-content-center align-items-center">
@@ -78,21 +113,24 @@ export function HomePage(){
         <input className="m-3" type="text" placeholder="Search here" onChange={(e) => {filteringPokemons(e.target.value)}} value={searchInput}/>
       </div>
 
-      <div>
+      <div className="d-lg-flex flex-column row g-3 m-5">
         {!errorMessagePokemons ? (
           filteredPokemons.map((item, index) => {
             return (
-              <div key={index}>
+              <div key={index} className="col-md-12">
                 <Card className="card mb-3">
-                    <Card.Body>
-                      <Card.Title className="title fs-6">
-                        Pokemon
-                      </Card.Title>
-                      <Card.Link href="#" onClick={() => viewPokemon(item.url)} className="subtitle fs-6 text-capitalize">
-                        {item.name}
-                      </Card.Link>
-                    </Card.Body>
-                  </Card>
+                  <Card.Body className="">
+                    <Card.Title className="title fs-6">
+                      Pokemon
+                    </Card.Title>
+                    <Card.Link href="#" onClick={() => viewPokemon(item.url, item.name)} className="subtitle fs-6 text-capitalize">
+                      {item.name}
+                    </Card.Link>
+                    <Card.Link>
+                      Link: {item.catch.toString()}
+                    </Card.Link>
+                  </Card.Body>
+                </Card>
               </div>
             )
           })
@@ -102,8 +140,19 @@ export function HomePage(){
       </div>
 
       <div>
-        <Modal>
-
+        <Modal show={show} onHide={modalClose} >
+          <Modal.Header closeButton>
+            <Modal.Title>{selectedPokemon.name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img src={selectedPokemon.image} />
+            <p>Weight: {selectedPokemon.weight}</p>
+            <p>Height: {selectedPokemon.height}</p>
+            <p>Not hidden abilities: {selectedPokemon.abilities}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button type="button" onClick={() => catchingPokemon(selectedPokemon.name)}>Catch</Button>
+          </Modal.Footer>
         </Modal>
       </div>
     </div>
